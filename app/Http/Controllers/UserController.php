dbae4d022cecdb698af7519c27c7668ee128ad23
aleprosli <aliff.rosli96@gmail.com>
2023-05-26 12:18:05 +0800

    use user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
@@ -13,7 +14,7 @@ class UserController extends Controller
 
         return response()->json([
             'message' => 'Semua list pengguna',
-            'data' => $users,
+            'data' => UserResource::collection($users),
             'code' => 200
         ]);
     }
